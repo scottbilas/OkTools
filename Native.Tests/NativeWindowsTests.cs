5b837174e15de5993d25614e936b8504bfaf25f2
Scott Bilas <scottbilas@gmail.com>
2022-02-23 17:09:20 +0100

    Added couple functions to get command line and working dir from another process

---
+++ Native.Tests/NativeWindowsTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using PInvoke;
+
+class NativeWindowsTests
+{
+    [Test]
+    public void GetProcessCurrentDirectory_WithValidProcessId_ReturnsCurrentDirectory()
+    {
+        // we're using npaths to normalize trailing slashes
+
+        var expected = Directory.GetCurrentDirectory().ToNPath();
+        var actual = NativeWindows.GetProcessCurrentDirectory(Process.GetCurrentProcess().Id)!.ToNPath();
+
+        actual.ShouldBe(expected);
+    }
+
+    // can't use Environment.CommandLine as it has been processed by .net and won't match
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    static extern IntPtr GetCommandLine();
+
+    [Test]
+    public void GetProcessCommandLine_WithValidProcessId_ReturnsCommandLine()
+    {
+        var expected = Marshal.PtrToStringAuto(GetCommandLine());
+        var actual = NativeWindows.GetProcessCommandLine(Process.GetCurrentProcess().Id);
+
+        actual.ShouldBe(expected);
+    }
+
+    [TestCase(0), TestCase(int.MaxValue)]
+    public void GetProcessX_WithInvalidProcessId_ThrowsInvalidParameter(int pid)
+    {
+        Should.Throw<Win32Exception>(() => NativeWindows.GetProcessCurrentDirectory(pid))
+            .NativeErrorCode.ShouldBe(Win32ErrorCode.ERROR_INVALID_PARAMETER);
+        Should.Throw<Win32Exception>(() => NativeWindows.GetProcessCommandLine(pid))
+            .NativeErrorCode.ShouldBe(Win32ErrorCode.ERROR_INVALID_PARAMETER);
+    }
+
+    [Test]
+    public void GetProcessX_WithSystemProcessId_ThrowsAccessDenied()
+    {
+        using var process = Process.GetProcessesByName("csrss")[0];
+
+        Should.Throw<Win32Exception>(() => NativeWindows.GetProcessCurrentDirectory(process.Id))
+            .NativeErrorCode.ShouldBe(Win32ErrorCode.ERROR_ACCESS_DENIED);
+        Should.Throw<Win32Exception>(() => NativeWindows.GetProcessCommandLine(process.Id))
+            .NativeErrorCode.ShouldBe(Win32ErrorCode.ERROR_ACCESS_DENIED);
+    }
+}
---
+++ Native/NativeWindows.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+using PInvoke;
+
+namespace OkTools.Native;
+
+[PublicAPI]
+public static class NativeWindows
+{
+    public static string? GetProcessCurrentDirectory(int processId) =>
+        GetProcessParametersString(processId, k_processCurrentDirectoryOffset);
+    public static string? GetProcessCommandLine(int processId) =>
+        GetProcessParametersString(processId, k_processCommandLineOffset);
+
+    // these constants from https://stackoverflow.com/a/23842609/14582
+    // internals stuff! may break in a new windows release! (unlikely! been this way for at least 10 years!)
+
+    const uint k_processParametersOffset       = 0x20; // windbg `dt ntdll!_PEB` and grep `ProcessParameters` to get offset from base
+    const uint k_processCurrentDirectoryOffset = 0x38; // undocumented
+    const uint k_processCommandLineOffset      = 0x70; // offset to winternl.h:RTL_USER_PROCESS_PARAMETERS::CommandLine
+
+    static unsafe string? GetProcessParametersString(int processId, uint offset)
+    {
+        if (sizeof(void*) != 8)
+            throw new NotSupportedException("Requires 64-bit OS");
+
+        var handle = Kernel32.OpenProcess(Kernel32.ProcessAccess.PROCESS_QUERY_INFORMATION | Kernel32.ProcessAccess.PROCESS_VM_READ, false, processId);
+        if (handle.IsInvalid)
+            throw new Win32Exception(Marshal.GetLastWin32Error());
+
+        try
+        {
+            var processBasicInformation = new NTDll.PROCESS_BASIC_INFORMATION();
+            NTDll.NtQueryInformationProcess(handle, NTDll.PROCESSINFOCLASS.ProcessBasicInformation,
+                &processBasicInformation, Marshal.SizeOf(processBasicInformation), out _).ThrowOnError();
+
+            byte* processParametersPtr;
+            if (!Kernel32.ReadProcessMemory(handle,
+                    (byte*)processBasicInformation.PebBaseAddress + k_processParametersOffset,
+                    &processParametersPtr, new UIntPtr((uint)sizeof(byte*)), out _))
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+
+            var processParameterUStr = new NTDll.UNICODE_STRING();
+            if (!Kernel32.ReadProcessMemory(handle,
+                    processParametersPtr + offset, &processParameterUStr, new UIntPtr((uint)Marshal.SizeOf(processParameterUStr)), out _))
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+
+            if (processParameterUStr.Buffer == null)
+                return null;
+            if (processParameterUStr.Length == 0)
+                return "";
+
+            var processParameterStr = new string('\0', processParameterUStr.Length / 2);
+            fixed (char* strBuffer = processParameterStr)
+            {
+                if (!Kernel32.ReadProcessMemory(handle,
+                        processParameterUStr.Buffer, strBuffer, new UIntPtr(processParameterUStr.Length), out _))
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+            return processParameterStr;
+        }
+        finally
+        {
+            handle.Close();
+        }
+    }
+}
