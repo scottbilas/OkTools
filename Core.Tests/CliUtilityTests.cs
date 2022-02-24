17f8d84f60c968e64ff767bc10f01b22b4ca7af9
Scott Bilas <scottbilas@gmail.com>
2022-02-24 15:15:47 +0100

    New toys

---
+++ Core.Tests/CliUtilityTests.cs
@@ -0,0 +1,49 @@
+﻿class CliUtilityTests
+{
+    [Test]
+    public void ParseCommandLineArgs_WithMixed_ShouldReturnAllUnquotedArgs()
+    {
+        var parsed = CliUtility.ParseCommandLineArgs(
+            @"""C:\path\with space\unity.exe"" -flag ""-quoted"" ""Arg With Space"" -another ""one last with space""");
+
+        parsed.ShouldBe(new[]
+        {
+            @"C:\path\with space\unity.exe",
+            "-flag",
+            "-quoted",
+            "Arg With Space",
+            "-another",
+            "one last with space"
+        });
+    }
+
+    [Test]
+    public void ParseCommandLineArgs_WithEmptyOrWhitespaceString_ShouldReturnEmptyCollection()
+    {
+        CliUtility.ParseCommandLineArgs("").ShouldBeEmpty();
+        CliUtility.ParseCommandLineArgs("    ").ShouldBeEmpty();
+    }
+
+    [Test]
+    public void CommandLineArgsToString_WithMixed_ShouldReturnAllUnquotedArgs()
+    {
+        var argsStr = CliUtility.CommandLineArgsToString(new[]
+        {
+            @"C:\path\with space\unity.exe",
+            "-flag",
+            "-quoted",
+            "Arg With Space",
+            "-another",
+            "one last with space"
+        });
+
+        argsStr.ShouldBe(@"""C:\path\with space\unity.exe"" -flag -quoted ""Arg With Space"" -another ""one last with space""");
+    }
+
+    [Test]
+    public void CommandLineArgsToString_WithEmptyOrWhitespaceStrings_ShouldReturnEmptyString()
+    {
+        CliUtility.CommandLineArgsToString(Array.Empty<string>()).ShouldBe("");
+        CliUtility.CommandLineArgsToString(new[] { "    ", "", " " }).ShouldBe("");
+    }
+}
--- Core/CliUtility.cs
+++ Core/CliUtility.cs
@@ -1,10 +1,27 @@
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace OkTools.Core;
 
 [PublicAPI]
 public static class CliUtility
 {
+    public static IEnumerable<string> ParseCommandLineArgs(string commandLine) => Regex
+        .Matches(commandLine, @"""(?<c>[^""]+)""|(?<c>[^\s""]+)")
+        .Select(r => r.Groups["c"].Value);
+
+    public static string CommandLineArgsToString(IEnumerable<string> args) => args
+        .Select(a =>
+        {
+            if (a.IsNullOrWhiteSpace())
+                return "";
+            if (a.Contains(' '))
+                return '"' + a + '"';
+            return a;
+        })
+        .StringJoin(" ")
+        .Trim();
+
     public static IEnumerable<string> SelectStdinLines()
     {
         for (;;)
