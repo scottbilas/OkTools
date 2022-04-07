39b2a24560900076b4ddc651b01448be5b90c3db
Scott Bilas <scottbilas@gmail.com>
2022-04-07 13:09:51 +0200

    First pass on reflowing

--- Core.Tests/CliUtilityTests.cs
+++ Core.Tests/CliUtilityTests.cs
@@ -46,4 +46,138 @@
         CliUtility.CommandLineArgsToString(Array.Empty<string>()).ShouldBe("");
         CliUtility.CommandLineArgsToString(new[] { "    ", "", " " }).ShouldBe("");
     }
+
+    [Test]
+    public void Reflow_Eols()
+    {
+        CliUtility.Reflow(
+            "some text\nother text\r\nstill more", 50).ShouldBe(
+            "some text\nother text\nstill more");
+        CliUtility.Reflow(
+            "some text\nother text\r\nstill more", 50, eol:"\r\n").ShouldBe(
+            "some text\r\nother text\r\nstill more");
+    }
+
+    [Test]
+    public void Reflow_TrimLineEnds()
+    {
+        CliUtility.Reflow(
+            "abc \n def ", 50).ShouldBe(
+            "abc\n def");
+    }
+
+    [Test]
+    public void Reflow_EmptyLines()
+    {
+        CliUtility.Reflow(
+            "\n\r\n\n", 50).ShouldBe(
+            "\n\n\n");
+        CliUtility.Reflow(
+            "", 50).ShouldBe(
+            "");
+    }
+
+    [Test]
+    public void Reflow_SoftWrap()
+    {
+        CliUtility.Reflow(
+            "this is a small sentence", 50).ShouldBe(
+            "this is a small sentence");
+        CliUtility.Reflow(
+            "this is a small sentence", 18).ShouldBe(
+            "this is a small\nsentence");
+        CliUtility.Reflow(
+            "this is a small sentence", 12).ShouldBe(
+            "this is a\nsmall\nsentence");
+    }
+
+    [Test]
+    public void Reflow_WithOverflow_DoesHardWrap()
+    {
+        CliUtility.Reflow(
+            "this has a really_long_word_in_it_yes_it_does", 12).ShouldBe(
+            "this has a\nreally_long_\nword_in_it_y\nes_it_does");
+    }
+
+    [Test]
+    public void Reflow_WithHardWrap()
+    {
+        CliUtility.Reflow(
+            "this has a difficult to wrap word in it", 9, 4).ShouldBe(
+            "this has\na difficu\nlt to\nwrap word\nin it");
+        CliUtility.Reflow(
+            "this has a difficult to wrap word in it", 10, 4).ShouldBe(
+            "this has a\ndifficult\nto wrap\nword in it");
+    }
+
+    [Test]
+    public void Reflow_WithSimpleContinuations()
+    {
+        CliUtility.Reflow(
+            "this is a line we are wrapping\nand another that should be joined to it", 12).ShouldBe(
+            "this is a\nline we are\nwrapping and\nanother that\nshould be\njoined to it");
+        CliUtility.Reflow(
+            "this is a line we are wrapping and\nanother that should be joined to it", 12).ShouldBe(
+            "this is a\nline we are\nwrapping and\nanother that\nshould be\njoined to it");
+    }
+
+    [Test]
+    public void Reflow_WithExistingWhitespaceBreaks()
+    {
+        // baseline
+        CliUtility.Reflow(
+            "this is a line we r wrapping", 12).ShouldBe(
+            "this is a\nline we r\nwrapping");
+        CliUtility.Reflow(
+            "this is a1 line we r wrapping", 12).ShouldBe(
+            "this is a1\nline we r\nwrapping");
+        CliUtility.Reflow(
+            "this is a12 line we r wrapping", 12).ShouldBe(
+            "this is a12\nline we r\nwrapping");
+        CliUtility.Reflow(
+            "this is a123 line we r wrapping", 12).ShouldBe(
+            "this is a123\nline we r\nwrapping");
+
+        // collapse whitespace when wrapped
+        CliUtility.Reflow(
+            "this is a      line we r     wrapping   ", 12).ShouldBe(
+            "this is a\nline we r\nwrapping");
+
+        // preserve whitespace when not wrapped
+        CliUtility.Reflow(
+            "this is a   line we r     wrapping   ", 18).ShouldBe(
+            "this is a   line\nwe r     wrapping");
+    }
+
+    [Test]
+    public void Reflow_WithSimpleIndentation()
+    {
+        CliUtility.Reflow(
+            "this line\nshould join", 50).ShouldBe(
+            "this line should join");
+        CliUtility.Reflow(
+            "  this line\n  should join", 50).ShouldBe(
+            "  this line should join");
+        CliUtility.Reflow(
+            "  this line\n  should join", 16).ShouldBe(
+            "  this line should\n  join");
+    }
+
+    [Test]
+    public void Reflow_WithInterruptedIndentation()
+    {
+        CliUtility.Reflow(
+            "this line\n\nshould join", 50).ShouldBe(
+            "this line\n\nshould join");
+        CliUtility.Reflow(
+            "  this line\n\n  should join", 50).ShouldBe(
+            "  this line\n\n  should join");
+        CliUtility.Reflow(
+            "  this line\n  \n  should join", 50).ShouldBe(
+            "  this line should join");
+
+        CliUtility.Reflow(
+            "  this line\n should not join\n ok but this\n  but no", 50).ShouldBe(
+            "  this line\n should not join ok but this\n  but no");
+    }
 }
--- Core/CliUtility.cs
+++ Core/CliUtility.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace OkTools.Core;
@@ -136,5 +137,150 @@ public static class CliUtility
             exePath, processArgs, workingDirectory,
             (line, stream) => (stream == StdStream.Stdout ? stdout : stderr).Add(line),
             stdin);
+    }
+
+    class Line
+    {
+        string _text;
+        int _start, _end;
+
+        public Line(string text) => _text = text;
+
+        public int Start
+        {
+            get => _start;
+            set
+            {
+                if (value < 0 || value > End)
+                    throw new Exception($"Line.Start: {value} < {0} || {value} > {End} (old Start: {_start})");
+                _start = value;
+            }
+        }
+
+        public int End
+        {
+            get => _end;
+            set
+            {
+                if (value < Start || value > _text.Length)
+                    throw new Exception($"Line.End: {value} < {Start} || {value} > {_text.Length} (old End: {_end})");
+                _end = value;
+            }
+        }
+
+        public int FindLine(int newStart)
+        {
+            // find end of this line
+            var end = _text.IndexOf('\n', _start = newStart);
+
+            // find actual end of this line
+            _end = end < 0 ? _text.Length : end;
+            _end = FindTrimEnd(_end);
+
+            // go to next line (or stay at end)
+            return end < 0 ? end : end+1;
+        }
+
+        public void TrimStart()
+        {
+            while (_end > _start && _text[_start] == ' ')
+                ++_start;
+        }
+
+        public int FindTrimEnd(int end)
+        {
+            while (end > _start && char.IsWhiteSpace(_text[end-1]))
+                --end;
+            return end;
+        }
+
+        public int Length => End - Start;
+
+        public override string ToString()
+        {
+            var text = _text.Substring(_start, Length)
+                .Replace("\r", "\\r")
+                .Replace("\n", "\\n");
+            return $"'{text}' (start={Start}, end={End}, len={Length})";
+        }
+    }
+
+    static readonly Regex k_TableRx = new(@"^ *[-*] |\b {2,}\b", RegexOptions.Multiline);
+
+    // wrapWidth: defaults to console width
+    // minWrapWidth: if a soft wrap takes the line width below this amount, then do a hard wrap instead
+    public static string Reflow(string text, int wrapWidth, int minWrapWidth = 0, string eol = "\n")
+    {
+        if (text.Contains('\t'))
+            throw new ArgumentException("Tab characters not ok in a reflow string");
+
+        var sb = new StringBuilder();
+        var currentLine = new Line(text);
+
+        var (lastLen, lastIndent, nextStart) = (0, 0, 0);
+
+        while (nextStart >= 0)
+        {
+            nextStart = currentLine.FindLine(nextStart);
+            if (currentLine.Length == 0)
+            {
+                sb.Append(eol);
+                continue;
+            }
+
+            var indent = text.IndexOfNot(' ', currentLine.Start, currentLine.Length);
+            currentLine.Start = indent;
+            var interruptedIndent = indent != lastIndent;
+            lastIndent = indent;
+
+            void Append(int len, bool terminate)
+            {
+                if (len > 0)
+                {
+                    if (lastLen > 0)
+                    {
+                        // if we were extending the previous line, add a space
+                        sb.Append(' ');
+                        ++lastLen;
+                    }
+                    else if (indent > 0)
+                    {
+                        // it's the start of a new line, so indent
+                        sb.Append(' ', indent);
+                        lastLen += indent;
+                    }
+
+                    // add line text
+                    var end = currentLine.FindTrimEnd(currentLine.Start + len);
+                    sb.Append(text, currentLine.Start, end - currentLine.Start);
+                    currentLine.Start += len;
+                    lastLen += len;
+
+                    // trim any following spaces so any further continuation wraps cleanly
+                    currentLine.TrimStart();
+                }
+
+                if (terminate)
+                {
+                    sb.Append(eol);
+                    lastLen = 0;
+                }
+            }
+
+            for (var available = wrapWidth - indent - lastLen - (lastLen > 0 ? 1 : 0);
+                 currentLine.Length > available; available = wrapWidth)
+            {
+                var wrapBreak = text.LastIndexOf(' ', currentLine.Start + available, available + 1);
+                if (wrapBreak >= currentLine.Start + minWrapWidth)
+                    Append(wrapBreak - currentLine.Start, true); // ok to wrap here
+                else
+                    Append(available, true); // too narrow, do a hard break
+            }
+
+            // write any remainder
+            Append(currentLine.Length, false);
+        }
+
+        return sb.ToString();
     }
 }
