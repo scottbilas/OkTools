dce15e2902eb9ff57db0afbdba9943582b31c971
Scott Bilas <scottbilas@gmail.com>
2022-05-31 17:06:14 +0200

    Getting test log generator going

--- Flog.Cli.Loggo/Program.cs
+++ Flog.Cli.Loggo/Program.cs
@@ -1,8 +1,7 @@
-﻿using System.Text;
-using DocoptNet;
+﻿using DocoptNet;
 using OkTools.Core;
 
-public static class Program
+public static partial class Program
 {
     const string k_programName = "loggo";
     const string k_docName = $"{k_programName}, the logfile generator";
@@ -14,23 +13,16 @@ Usage:
   {k_programName}  --version
 
 Options:
-  --speed MINMAX  Delay between each line being added to the log in ms [default: 1,5]
-  --size MINMAX   Will stop generating after log gets to this size (can postfix SIZE with kb mb gb) [default: 100MB]
-  --width MINMAX  Width of generated lines [default: 1,200]
+  --stdout           Mirror to stdout as well
+  --speed MINMAX     Delay between each line being added to the log in ms [default: 1,5]
+  --width MINMAX     Width of generated lines [default: 1,200]
+  --size SIZE        Will stop generating after log gets to this size (can postfix SIZE with kb mb gb) [default: 100MB]
+  --pattern PATTERN  Pattern to use for generated lines (one of: lorem, nums, loggy) [default: nums]
+  --eol EOL          End of line character to use (one of: crlf, lf, mix) [default: lf]
 
 Notes:
   MINMAX can be min and max entries separated by a comma (for example `1MB,50GB`). Both min and max will be the same if there is only one entry.
 ";
-
-    static (float min, float max) ParseMinMax(string str)
-    {
-
-    }
-
-    static (long min, long max) ParseMinMaxSize(string str)
-    {
-
-    }
 
     public static int Main(string[] args)
     {
@@ -41,11 +33,7 @@ Notes:
 
             var opt = new Docopt().Apply(k_docUsage, args, version: $"{k_docName} {k_docVersion}", help: false);
 
-            return (int)LoggIt(
-                ParseMinMax(opt["--speed"].ToString()),
-                ParseMinMaxSize(opt["--size"].ToString()),
-                ParseMinMax(opt["--width"].ToString()),
-                opt["PATH"].ToString());
+            return (int)LoggIt(opt);
         }
         catch (DocoptInputErrorException x)
         {
@@ -67,17 +55,63 @@ Notes:
         }
     }
 
-    static CliExitCode LoggIt(
-        (float min, float max) speed,
-        (long min, long max) size,
-        (float min, float max) width,
-        string path)
+    static CliExitCode LoggIt(IDictionary<string, ValueObject> options)
     {
-        using var writer = File.CreateText(path);
-        var sb = new StringBuilder();
+        using var writer = File.CreateText(options["PATH"].ToString());
 
-        var written = 0;
+        var stdout = options["--stdout"].IsTrue;
+        var speed = ParseMinMaxFloat(options["--speed"].ToString());
+        var width = ParseMinMaxInt(options["--width"].ToString());
+        var size = ParseSize(options["--size"].ToString());
+        var pattern = options["--pattern"].ToString();
+        var eol = options["--eol"].ToString();
 
+        var csb = new CharSpanBuilder(width.max + 2); // room for \r\n
+
+        for (var (written, line) = (0L, 0); written < size; ++line)
+        {
+            csb.Clear();
+
+            switch (pattern)
+            {
+                case "lorem":
+                    throw new NotImplementedException();
+                case "nums":
+                    csb.Append(line);
+                    break;
+                case "loggy":
+                    throw new NotImplementedException();
+                default:
+                    throw new DocoptInputErrorException($"Invalid --pattern option `{pattern}`");
+            }
+
+            switch (eol)
+            {
+                case "crlf":
+                    csb.Append("\r\n");
+                    break;
+                case "lf":
+                    csb.Append('\n');
+                    break;
+                case "mix":
+                    csb.Append(Random.Shared.Next() % 2 == 0 ? "\r\n" : "\n");
+                    break;
+                default:
+                    throw new DocoptInputErrorException($"Invalid --eol option `{eol}`");
+            }
+
+            writer.Write(csb.Span);
+            written += csb.Span.Length;
+
+            if (stdout)
+            {
+                var (chars, count) = csb.Chars;
+                Console.Write(chars, 0, count);
+            }
+
+            var delay = Random.Shared.NextSingle() * (speed.max - speed.min) + speed.min;
+            Thread.Sleep((int)(delay / 1000));
+        }
 
         return CliExitCode.Success;
     }
---
+++ Flog.Cli.Loggo/Program_Parse.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+public static partial class Program
+{
+    static (float min, float max) ParseMinMaxFloat(string str)
+    {
+        var split = str.Split(',', 2);
+        var min = float.Parse(split[0]);
+        var max = split.Length == 2 ? float.Parse(split[1]) : min;
+        return (min, max);
+    }
+
+    static (int min, int max) ParseMinMaxInt(string str)
+    {
+        var split = str.Split(',', 2);
+        var min = int.Parse(split[0]);
+        var max = split.Length == 2 ? int.Parse(split[1]) : min;
+        return (min, max);
+    }
+
+    static long ParseSize(string str)
+    {
+        var match = Regex.Match(str, @"(\d+)([kmg]b)?", RegexOptions.IgnoreCase);
+        if (!match.Success)
+            throw new ArgumentException($"`{str}` is not a valid size");
+
+        var multiplier = 1;
+        if (match.Groups[2].Success)
+        {
+            multiplier *= match.Groups[2].Value[0] switch
+            {
+                'k' => 1024,
+                'm' => 1024 * 1024,
+                'g' => 1024 * 1024 * 1024,
+                _ => throw new Exception("Regex fail")
+            };
+        }
+
+        return long.Parse(match.Groups[1].Value) * multiplier;
+    }
+}
