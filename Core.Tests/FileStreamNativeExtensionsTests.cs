43362aa75e59ac13d2f70cdc03ab1f81d2ff01ab
Scott Bilas <scottbilas@gmail.com>
2022-06-09 14:28:43 +0200

    New FileStream.WasFileDeleted extension, plus tests

---
+++ Core.Tests/FileStreamNativeExtensionsTests.cs
@@ -0,0 +1,61 @@
+class FileStreamNativeExtensionsTests : TestFileSystemFixture
+{
+    NPath _path = null!;
+    FileStream _stream = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _path = BaseDir.Combine("test.dat");
+        _path.DeleteIfExists();
+        _path.WriteAllText(TestContext.CurrentContext.Test.Name);
+        _stream = new FileStream(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _path.DeleteIfExists();
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (_stream != null)
+        {
+            _stream.Dispose();
+            _stream = null!;
+        }
+    }
+
+    [Test]
+    public void WasFileDeleted_WithExistingFile_ReturnsFalse()
+    {
+        _stream.WasFileDeleted().ShouldBeFalse();
+    }
+
+    [Test]
+    public void WasFileDeleted_WithDeletedFile_ReturnsTrue()
+    {
+        _path.Delete();
+        _stream.WasFileDeleted().ShouldBeTrue();
+    }
+
+    [Test]
+    public void WasFileDeleted_WithOverwrittenFile_ReturnsFalse() // overwriting a file does not delete it first
+    {
+        _path.WriteAllText("Overwritten!");
+        _stream.WasFileDeleted().ShouldBeFalse();
+    }
+
+    [Test]
+    public void WasFileDeleted_WithDeletedAndRecreatedFile_ReturnsTrue()
+    {
+        _path.Delete();
+        _path.WriteAllText("Recreated!");
+        _stream.WasFileDeleted().ShouldBeTrue();
+    }
+
+    [Test]
+    public void WasFileDeleted_WithClosedFile_Throws()
+    {
+        _stream.Close();
+        Should.Throw<ObjectDisposedException>(() => _stream.WasFileDeleted());
+    }
+}
---
+++ Core/FileStreamNativeExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace OkTools.Core;
+
+public static class FileStreamNativeExtensions
+{
+    // originally from https://stackoverflow.com/a/54657277/14582
+
+    // TODO: contribute upstream to https://github.com/dotnet/pinvoke/tree/main/src/Kernel32
+    static class Native
+    {
+        // ReSharper disable InconsistentNaming
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetFileInformationByHandleEx(IntPtr hFile, int FileInformationClass, IntPtr lpFileInformation, uint dwBufferSize);
+
+        public struct FILE_STANDARD_INFO
+        {
+            #pragma warning disable CS0649
+            public long AllocationSize;
+            public long EndOfFile;
+            public uint NumberOfLinks;
+            public byte DeletePending;
+            public byte Directory;
+            #pragma warning restore CS0649
+        }
+
+        public const int FileStandardInfo = 1;
+
+        // ReSharper restore InconsistentNaming
+    }
+
+    // use this when opening a FileStream with FileShare.Delete. if someone deletes the underlying file, the stream will
+    // just report that it's at eof and give no errors, but this function will detect that case.
+    public static bool WasFileDeleted(this FileStream @this)
+    {
+        var size = Marshal.SizeOf<Native.FILE_STANDARD_INFO>();
+        var buf = Marshal.AllocHGlobal(size);
+        
+        try
+        {
+            var handle = @this.SafeFileHandle.DangerousGetHandle();
+            if (!Native.GetFileInformationByHandleEx(handle, Native.FileStandardInfo, buf, (uint)size))
+                throw new Win32Exception("GetFileInformationByHandleEx() failed");
+
+            var info = Marshal.PtrToStructure<Native.FILE_STANDARD_INFO>(buf);
+            return info.DeletePending != 0;
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(buf);
+        }
+    }
+}
