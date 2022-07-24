5b79cab0e0b1e79f5a128aef4ebb3c67578252bf
Scott Bilas <scottbilas@gmail.com>
2022-07-24 21:37:23 +0100

    New (RO)Span extension methods and tests

---
+++ Core/SpanExtensions.gen.cs
@@ -0,0 +1,65 @@
+﻿namespace OkTools.Core;
+
+public static class ReadOnlySpanExtensions
+{
+    public static ReadOnlySpan<T> SafeSlice<T>(this ReadOnlySpan<T> @this, int start, int length)
+    {
+        if (start < 0)
+        {
+            length += start;
+            start = 0;
+        }
+
+        if (length <= 0)
+            return default;
+
+        if (start + length > @this.Length)
+            length = @this.Length - start;
+
+        if (length <= 0)
+            return default;
+
+        return @this.Slice(start, length);
+    }
+
+    public static ReadOnlySpan<T> SafeSlice<T>(this ReadOnlySpan<T> @this, int start)
+    {
+        if (start < 0)
+            start = 0;
+        else if (start >= @this.Length)
+            return default;
+
+        return @this[start..];
+    }
+}
+
+public static class SpanExtensions
+{
+    public static Span<T> SafeSlice<T>(this Span<T> @this, int start, int length)
+    {
+        if (start < 0)
+        {
+            length += start;
+            start = 0;
+        }
+
+        if (length <= 0)
+            return default;
+
+        if (start + length > @this.Length)
+            length = @this.Length - start;
+
+        return @this.Slice(start, length);
+    }
+
+    public static Span<T> SafeSlice<T>(this Span<T> @this, int start)
+    {
+        if (start < 0)
+            start = 0;
+        else if (start >= @this.Length)
+            return default;
+
+        return @this[start..];
+    }
+}
+
