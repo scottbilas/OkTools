860c3714fa7c5c557c89bf12494c0973a4d8c833
Scott Bilas <scottbilas@gmail.com>
2022-08-23 22:01:45 +0200

    Start migrating to common docopt helper code

--- src/Flog.Cli/FlogCli.cs
+++ src/Flog.Cli/FlogCli.cs
@@ -1,9 +1,7 @@
-﻿using DocoptNet;
+﻿const string programVersion = "0.1";
 
-// TODO: this is temporary until remove Console.* calls
+// fine to use console directly here, it's outside of any tui work
 #pragma warning disable RS0030
-
-const string programVersion = "0.1";
 
 // TODO: add --config prefix before command
 
@@ -11,64 +9,18 @@ var debugMode = false;
 
 try
 {
-    var result = FlogCliArguments.CreateParser()
-        .WithVersion(programVersion)
-        .Parse(args);
-
-    switch (result)
-    {
-        case IArgumentsResult<FlogCliArguments> cliArgs:
+    return (int)await FlogCliArguments.CreateParser().Parse(
+        args,
+        programVersion,
+        FlogCliArguments.Help,
+        FlogCliArguments.Usage,
+        cliArgs =>
         {
-            debugMode = cliArgs.Arguments.OptDebug;
+            debugMode = cliArgs.OptDebug;
 
-            using var flogApp = new FlogApp(cliArgs.Arguments);
-            return (int)await flogApp.Run();
-        }
-        case IHelpResult:
-        {
-            var helpText = FormatHelp(FlogCliArguments.Help);
-            Console.WriteLine(DocoptUtility.Reflow(helpText, Console.WindowWidth));
-            return (int)CliExitCode.Help;
-        }
-        case IVersionResult:
-        {
-            var shortDescription = FormatHelp(FlogCliArguments.Help[..FlogCliArguments.Help.IndexOf('\n')].Trim());
-            Console.WriteLine(shortDescription);
-            return (int)CliExitCode.Help;
-        }
-        case IInputErrorResult errorResult:
-        {
-            var printed = false;
-
-            if (args.Length != 0)
-            {
-                Console.Error.WriteLine("Bad command line: " + args.StringJoin(' '));
-                printed = true;
-            }
-
-            if (errorResult.Error.Any())
-            {
-                Console.Error.WriteLine(errorResult.Error);
-                printed = true;
-            }
-
-            if (printed)
-                Console.Error.WriteLine();
-
-            var usageText = FormatHelp(FlogCliArguments.Usage);
-            Console.Error.WriteLine(DocoptUtility.Reflow(usageText, Console.WindowWidth));
-
-            return (int)CliExitCode.ErrorUsage;
-        }
-        default:
-            throw new InvalidOperationException($"Unexpected result type {result.GetType().FullName}");
-    }
-
-    static string FormatHelp(string helpText)
-    {
-        var programName = Path.GetFileNameWithoutExtension(Environment.ProcessPath!);
-        return string.Format(helpText, programName, programVersion);
-    }
+            using var flogApp = new FlogApp(cliArgs);
+            return flogApp.Run();
+        });
 }
 catch (CliExitException x)
 {
@@ -85,11 +37,4 @@ catch (CliExitException x)
         Console.Error.WriteLine(x);
 
     return (int)x.Code;
-}
-catch (Exception x)
-{
-    Console.Error.WriteLine("Internal error!");
-    Console.Error.WriteLine();
-    Console.Error.WriteLine(x);
-    return (int)CliExitCode.ErrorSoftware;
 }
---
+++ src/_Include/DocoptExtensions.cs
@@ -0,0 +1,84 @@
+using DocoptNet;
+using OkTools.Core;
+
+// fine to use console directly here, it's outside of any tui work
+#pragma warning disable RS0030
+
+// ReSharper disable MethodHasAsyncOverload
+
+static class DocoptExtensions
+{
+    public static async Task<CliExitCode> Parse<T>(this IHelpFeaturingParser<T> @this,
+        IReadOnlyCollection<string> args,
+        string programVersion,
+        string help,
+        string usage,
+        Func<T, Task<CliExitCode>> mainFunc)
+    {
+        try
+        {
+            var result = @this
+                .WithVersion(programVersion)
+                .Parse(args);
+
+            switch (result)
+            {
+                case IArgumentsResult<T> cliArgs:
+                {
+                    return await mainFunc(cliArgs.Arguments);
+                }
+                case IHelpResult:
+                {
+                    var helpText = FormatHelp(help, programVersion);
+                    Console.WriteLine(DocoptUtility.Reflow(helpText, Console.WindowWidth));
+                    return CliExitCode.Help;
+                }
+                case IVersionResult:
+                {
+                    var shortDescription = FormatHelp(help[..help.IndexOf('\n')].Trim(), programVersion);
+                    Console.WriteLine(shortDescription);
+                    return CliExitCode.Help;
+                }
+                case IInputErrorResult errorResult:
+                {
+                    var printed = false;
+
+                    if (args.Count != 0)
+                    {
+                        Console.Error.WriteLine("Bad command line: " + args.StringJoin(' '));
+                        printed = true;
+                    }
+
+                    if (Enumerable.Any(errorResult.Error))
+                    {
+                        Console.Error.WriteLine(errorResult.Error);
+                        printed = true;
+                    }
+
+                    if (printed)
+                        Console.Error.WriteLine();
+
+                    var usageText = FormatHelp(usage, programVersion);
+                    Console.Error.WriteLine(DocoptUtility.Reflow(usageText, Console.WindowWidth));
+
+                    return CliExitCode.ErrorUsage;
+                }
+                default:
+                    throw new InvalidOperationException($"Unexpected result type {result.GetType().FullName}");
+            }
+
+            static string FormatHelp(string helpText, string version)
+            {
+                var programName = Path.GetFileNameWithoutExtension(Environment.ProcessPath!);
+                return string.Format(helpText, programName, version);
+            }
+        }
+        catch (Exception x)
+        {
+            Console.Error.WriteLine("Internal error!");
+            Console.Error.WriteLine();
+            Console.Error.WriteLine(x);
+            return CliExitCode.ErrorSoftware;
+        }
+    }
+}
