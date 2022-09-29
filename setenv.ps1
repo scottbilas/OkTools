0ed01edd4d7451a6b99b35c0365a9037f15365e0
Scott Bilas <scottbilas@gmail.com>
2022-09-29 13:04:17 +0200

    Add support for both release and debug aliases

--- setenv.ps1
+++ setenv.ps1
@@ -3,7 +3,12 @@
 # TODO (alternate): eliminate need to dot-source
 'Building..'
 dotnet build --nologo -v q
-set-alias okflog $PSScriptRoot\build\Flog.Cli\Debug\okflog.exe
-set-alias okunity $PSScriptRoot\build\Unity.Cli\Debug\okunity.exe
-set-alias loggo $PSScriptRoot\build\Loggo.Cli\Debug\loggo.exe
-set-alias pmltool $PSScriptRoot\build\PmlTool.Cli\Debug\pmltool.exe
+dotnet build -c Release --nologo -v q
+set-alias okflogd $PSScriptRoot\build\Flog.Cli\Debug\okflog.exe
+set-alias okunityd $PSScriptRoot\build\Unity.Cli\Debug\okunity.exe
+set-alias loggod $PSScriptRoot\build\Loggo.Cli\Debug\loggo.exe
+set-alias pmltoold $PSScriptRoot\build\PmlTool.Cli\Debug\pmltool.exe
+set-alias okflog $PSScriptRoot\build\Flog.Cli\okflog.exe
+set-alias okunity $PSScriptRoot\build\Unity.Cli\okunity.exe
+set-alias loggo $PSScriptRoot\build\Loggo.Cli\loggo.exe
+set-alias pmltool $PSScriptRoot\build\PmlTool.Cli\pmltool.exe
