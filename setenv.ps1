1dc0c9b941e3acfa8e49c525ab894b653b0be106
Scott Bilas <scottbilas@gmail.com>
2022-10-03 09:17:01 +0200

    Warn if not dot-sourced
    Also switch to an alias I use more often

--- setenv.ps1
+++ setenv.ps1
@@ -1,14 +1,18 @@
-# dot source me!
-# TODO: detect and warn if not dot-sourced
-# TODO (alternate): eliminate need to dot-source
+$isDotSourced = $MyInvocation.InvocationName -eq '.' -or $MyInvocation.Line -eq ''
+# TODO eliminate need to dot-source
+if (!$isDotSourced) {
+    # https://stackoverflow.com/a/33855217/14582
+    throw 'This script needs to be dot-sourced'
+}
+
 'Building..'
 dotnet build --nologo -v q
 dotnet build -c Release --nologo -v q
 set-alias okflogd $PSScriptRoot\build\Flog.Cli\Debug\okflog.exe
-set-alias okunityd $PSScriptRoot\build\Unity.Cli\Debug\okunity.exe
+set-alias okud $PSScriptRoot\build\Unity.Cli\Debug\okunity.exe
 set-alias loggod $PSScriptRoot\build\Loggo.Cli\Debug\loggo.exe
 set-alias pmltoold $PSScriptRoot\build\PmlTool.Cli\Debug\pmltool.exe
 set-alias okflog $PSScriptRoot\build\Flog.Cli\okflog.exe
-set-alias okunity $PSScriptRoot\build\Unity.Cli\okunity.exe
+set-alias oku $PSScriptRoot\build\Unity.Cli\okunity.exe
 set-alias loggo $PSScriptRoot\build\Loggo.Cli\loggo.exe
 set-alias pmltool $PSScriptRoot\build\PmlTool.Cli\pmltool.exe
