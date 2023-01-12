fa4e6b05fc3507597a52c26b61b082b26197419d
Scott Bilas <scottbilas@gmail.com>
2023-01-12 16:00:59 +0100

    Bit of cleanup

--- src/Unity/AssetDb/PropertyDefinition.cs
+++ src/Unity/AssetDb/PropertyDefinition.cs
@@ -39,65 +39,34 @@ public class PropertyDefinition
         throw new InvalidDataException($"Unknown property: {Encoding.ASCII.GetString(name)}");
     }
 
-    static readonly PropertyDefinition[] k_all =
+    static readonly PropertyDefinition[] k_all = new PropertyDefinition[]
     {
-        // from top of PropertyDefinition.cpp
-        /*StringArrayPropertyDefinition kLabelsPropDef*/                         new("labels",                          true,  LmdbValue.Type.StringArray),
-        /*SInt32PropertyDefinition kAssetBundleIndexPropDef*/                    new("AssetBundleIndex",                false, LmdbValue.Type.SInt32),
-        /*StringPropertyDefinition kAssetBundleNamePropDef*/                     new("assetBundleName",                 true,  LmdbValue.Type.String),
-        /*StringPropertyDefinition kAssetBundleVariantPropDef*/                  new("assetBundleVariant",              true,  LmdbValue.Type.String),
-        /*SInt64PropertyDefinition kMainObjectLocalIdentifierInFilePropDef*/     new("MainObjectLocalIdentifierInFile", false, LmdbValue.Type.SInt64),
-        /*ImporterIDPropertyDefinition kImporterOverridePropDef*/                new("importerOverride",                true,  LmdbValue.Type.ImporterId),
-        /*Hash128PropertyDefinition kImportLogFilePropDef*/                      new("importLogFile",                   false, LmdbValue.Type.Hash128),
-        /*SInt32PairPropertyDefinition kImportLogEntriesCountPropDef*/           new("ImportLogEntriesCount",           false, LmdbValue.Type.SInt32Pair),
-        /*StringPropertyDefinition kScriptCompilationAssetPathPropDef*/          new("scriptCompilationAssetPath",      false, LmdbValue.Type.String),
-        /*Hash128PropertyDefinition kImporterErrorFilePropDef*/                  new("importerErrorFile",               false, LmdbValue.Type.Hash128),
-        /*SInt32PropertyDefinition kAssetOriginProductIdPropDef*/                new("productId",                       true,  LmdbValue.Type.SInt32),
-        /*StringPropertyDefinition kAssetOriginPackageNamePropDef*/              new("packageName",                     true,  LmdbValue.Type.String),
-        /*StringPropertyDefinition kAssetOriginPackageVersionPropDef*/           new("packageVersion",                  true,  LmdbValue.Type.String),
-        /*StringPropertyDefinition kAssetOriginAssetPathPropDef*/                new("assetPath",                       true,  LmdbValue.Type.String),
-        /*SInt32PropertyDefinition kAssetOriginUploadIdPropDef*/                 new("uploadId",                        true,  LmdbValue.Type.SInt32),
+        // from PropertyDefinition.cpp
+        /*StringArrayPropertyDefinition kLabelsPropDef*/                          new("labels",                          LmdbValue.Type.StringArray),
+        /*SInt32PropertyDefinition kAssetBundleIndexPropDef*/                     new("AssetBundleIndex",                LmdbValue.Type.SInt32),
+        /*StringPropertyDefinition kAssetBundleNamePropDef*/                      new("assetBundleName",                 LmdbValue.Type.String),
+        /*StringPropertyDefinition kAssetBundleVariantPropDef*/                   new("assetBundleVariant",              LmdbValue.Type.String),
+        /*SInt64PropertyDefinition kMainObjectLocalIdentifierInFilePropDef*/      new("MainObjectLocalIdentifierInFile", LmdbValue.Type.SInt64),
+        /*ImporterIDPropertyDefinition kImporterOverridePropDef*/                 new("importerOverride",                LmdbValue.Type.ImporterId),
+        /*Hash128PropertyDefinition kImportLogFilePropDef*/                       new("importLogFile",                   LmdbValue.Type.Hash128),
+        /*SInt32PairPropertyDefinition kImportLogEntriesCountPropDef*/            new("ImportLogEntriesCount",           LmdbValue.Type.SInt32Pair),
+        /*StringPropertyDefinition kScriptCompilationAssetPathPropDef*/           new("scriptCompilationAssetPath",      LmdbValue.Type.String),
+        /*Hash128PropertyDefinition kImporterErrorFilePropDef*/                   new("importerErrorFile",               LmdbValue.Type.Hash128),
+
+        /*SInt32PropertyDefinition kAssetOriginProductIdPropDef*/                 new("productId",                       LmdbValue.Type.SInt32),
+        /*StringPropertyDefinition kAssetOriginPackageNamePropDef*/               new("packageName",                     LmdbValue.Type.String),
+        /*StringPropertyDefinition kAssetOriginPackageVersionPropDef*/            new("packageVersion",                  LmdbValue.Type.String),
+        /*StringPropertyDefinition kAssetOriginAssetPathPropDef*/                 new("assetPath",                       LmdbValue.Type.String),
+        /*SInt32PropertyDefinition kAssetOriginUploadIdPropDef*/                  new("uploadId",                        LmdbValue.Type.SInt32),
 
         // pending in assetpipeline/no-script-import
-        /*BoolPropertyDefinition kAssetRequireImportFlagPropDef*/                new("assetReqImport",                  false, LmdbValue.Type.Bool),
-        /*BoolPropertyDefinition kManagedPluginFlagPropDef*/                     new("managedPlugin",                   false, LmdbValue.Type.Bool),
-        /*NamedPPtrArrayPropertyDefinition kMonoScriptDefaultReferencesPropDef*/ new("MonoImporter.defaultReferences",  true,  LmdbValue.Type.NamedPPtrArray),
-        /*BlobPPtrPropertyDefinition kMonoScriptIconPropDef*/                    new("MonoImporter.icon",               true,  LmdbValue.Type.BlobPPtr),
-        /*SInt32PropertyDefinition kMonoScriptExecutionOrderPropDef*/            new("MonoImporter.executionOrder",     true,  LmdbValue.Type.SInt32),
-
-
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef0",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef1",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef2",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef3",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef4",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef5",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef6",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef7",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef8",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef9",            false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef10",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef11",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef12",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef13",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef14",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef15",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef16",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef17",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef18",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef19",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef20",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef21",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef22",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef23",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef24",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef25",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef26",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef27",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef28",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef29",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef30",           false, LmdbValue.Type.Hash128),
-        /*Hash128PropertyDefinition*/                                            new("ArtifactFilePropDef31",           false, LmdbValue.Type.Hash128),
-
-    };
+        /*BoolPropertyDefinition kAssetRequireImportFlagPropDef*/                new("assetReqImport",                   LmdbValue.Type.Bool),
+        /*BoolPropertyDefinition kManagedPluginFlagPropDef*/                     new("managedPlugin",                    LmdbValue.Type.Bool),
+        /*StringRefPropertyDefinition kNativePluginPathPropDef*/                 new("nativePluginPath",                 LmdbValue.Type.String),
+        /*NamedPPtrArrayPropertyDefinition kMonoScriptDefaultReferencesPropDef*/ new("MonoImporter.defaultReferences",   LmdbValue.Type.NamedPPtrArray),
+        /*BlobPPtrPropertyDefinition kMonoScriptIconPropDef*/                    new("MonoImporter.icon",                LmdbValue.Type.BlobPPtr),
+        /*SInt32PropertyDefinition kMonoScriptExecutionOrderPropDef*/            new("MonoImporter.executionOrder",      LmdbValue.Type.SInt32)
+    }
+    .Concat(Enumerable.Range(0, 32).Select(i => /*Hash128PropertyDefinition*/ new PropertyDefinition("ArtifactFilePropDef" + i, LmdbValue.Type.Hash128)))
+    .ToArray();
 }
