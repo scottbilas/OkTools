c0a870b04b21641852d17577cdea5d1895d6dcce
Scott Bilas <scottbilas@gmail.com>
2023-06-14 14:48:32 +0200

    Start experimental git commands stream

---
+++ src/Unity.Cli/Commands_RunGit.cs
@@ -0,0 +1,24 @@
+﻿static partial class Commands
+{
+    public const string DocUsageGit =
+@"Usage:
+  okunity git sparse
+
+Commands:
+  sparse  Update checkout by updating sparse spec from config
+";
+
+    public static CliExitCode RunGit(CommandContext context)
+    {
+        if (context.CommandLine["sparse"].IsTrue)
+        {
+        }
+
+        // shouldn't get here
+        return CliExitCode.ErrorUsage;
+    }
+}
+
+class MonoRepoSpec
+{
+}
