c298bc7ea10f2a486b0a63629d76f5be3fb83d07
Scott Bilas <scottbilas@gmail.com>
2023-11-10 15:01:04 +0100

    Add docs to CliExitCode

--- src/Core/CliExitCode.cs
+++ src/Core/CliExitCode.cs
@@ -1,5 +1,12 @@
 ﻿namespace OkTools.Core;
 
+/// <summary>
+/// Mnemonics for unixy process exit codes.
+/// <list type="bullet">
+///   <item>sysexit codes: https://man.openbsd.org/sysexits</item>
+///   <item>bash codes: https://tldp.org/LDP/abs/html/exitcodes.html</item>
+/// </list>
+/// </summary>
 [PublicAPI]
 public enum CliExitCode
 {
@@ -9,27 +16,44 @@ public enum CliExitCode
     #pragma warning restore CA1069
     ErrorGeneral = 1,
 
-    // https://man.openbsd.org/sysexits
+    // Sysexit codes
 
-    ErrorUsage       = 64, // The command was used incorrectly, e.g., with the wrong number of arguments, a bad flag, bad syntax in a parameter, or whatever.
-    ErrorDataErr     = 65, // The input data was incorrect in some way. This should only be used for user's data and not system files.
-    ErrorNoInput     = 66, // An input file (not a system file) did not exist or was not readable. This could also include errors like “No message” to a mailer (if it cared to catch it).
-    ErrorNoUser      = 67, // The user specified did not exist. This might be used for mail addresses or remote logins.
-    ErrorNoHost      = 68, // The host specified did not exist. This is used in mail addresses or network requests.
-    ErrorUnavailable = 69, // A service is unavailable. This can occur if a support program or file does not exist. This can also be used as a catch-all message when something you wanted to do doesn't work, but you don't know why.
-    ErrorSoftware    = 70, // An internal software error has been detected. This should be limited to non-operating system related errors if possible.
-    ErrorOsErr       = 71, // An operating system error has been detected. This is intended to be used for such things as “cannot fork”, or “cannot create pipe”. It includes things like getuid(2) returning a user that does not exist in the passwd file.
-    ErrorOsFile      = 72, // Some system file (e.g., /etc/passwd, /var/run/utmp) does not exist, cannot be opened, or has some sort of error (e.g., syntax error).
-    ErrorCantCreate  = 73, // A (user specified) output file cannot be created.
-    ErrorIoErr       = 74, // An error occurred while doing I/O on some file.
-    ErrorTempFail    = 75, // Temporary failure, indicating something that is not really an error. For example that a mailer could not create a connection, and the request should be reattempted later.
-    ErrorProtocol    = 76, // The remote system returned something that was “not possible” during a protocol exchange.
-    ErrorNoPerm      = 77, // You did not have sufficient permission to perform the operation. This is not intended for file system problems, which should use EX_NOINPUT or EX_CANTCREAT, but rather for higher level permissions.
-    ErrorConfig      = 78, // Something was found in an unconfigured or misconfigured state.
+    /// <summary>The command was used incorrectly, e.g., with the wrong number of arguments, a bad flag, bad syntax in a parameter, or whatever.</summary>
+    ErrorUsage       = 64,
+    /// <summary>The input data was incorrect in some way. This should only be used for user's data and not system files.</summary>
+    ErrorDataErr     = 65,
+    /// <summary>An input file (not a system file) did not exist or was not readable. This could also include errors like “No message” to a mailer (if it cared to catch it).</summary>
+    ErrorNoInput     = 66,
+    /// <summary>The user specified did not exist. This might be used for mail addresses or remote logins.</summary>
+    ErrorNoUser      = 67,
+    /// <summary>The host specified did not exist. This is used in mail addresses or network requests.</summary>
+    ErrorNoHost      = 68,
+    /// <summary>A service is unavailable. This can occur if a support program or file does not exist. This can also be used as a catch-all message when something you wanted to do doesn't work, but you don't know why.</summary>
+    ErrorUnavailable = 69,
+    /// <summary>An internal software error has been detected. This should be limited to non-operating system related errors if possible.</summary>
+    ErrorSoftware    = 70,
+    /// <summary>An operating system error has been detected. This is intended to be used for such things as “cannot fork”, or “cannot create pipe”. It includes things like getuid(2) returning a user that does not exist in the passwd file.</summary>
+    ErrorOsErr       = 71,
+    /// <summary>Some system file (e.g., /etc/passwd, /var/run/utmp) does not exist, cannot be opened, or has some sort of error (e.g., syntax error).</summary>
+    ErrorOsFile      = 72,
+    /// <summary>A (user specified) output file cannot be created.</summary>
+    ErrorCantCreate  = 73,
+    /// <summary>An error occurred while doing I/O on some file.</summary>
+    ErrorIoErr       = 74,
+    /// <summary>Temporary failure, indicating something that is not really an error. For example that a mailer could not create a connection, and the request should be reattempted later.</summary>
+    ErrorTempFail    = 75,
+    /// <summary>The remote system returned something that was “not possible” during a protocol exchange.</summary>
+    ErrorProtocol    = 76,
+    /// <summary>You did not have sufficient permission to perform the operation. This is not intended for file system problems, which should use EX_NOINPUT or EX_CANTCREAT, but rather for higher level permissions.</summary>
+    ErrorNoPerm      = 77,
+    /// <summary>Something was found in an unconfigured or misconfigured state.</summary>
+    ErrorConfig      = 78,
 
-    // https://tldp.org/LDP/abs/html/exitcodes.html
+    // Bash codes
 
+    /// <summary>Permission problem or command is not an executable</summary>
     ErrorCommandInvokedCannotExecute = 126,
+    /// <summary>Possible problem with $PATH or a typo</summary>
     ErrorCommandNotFound = 127,
 //  ErrorSignal = 128, // use FromSignal below
 }
@@ -64,11 +88,10 @@ public static class UnixSignalUtils
 
 public class CliErrorException : Exception
 {
+    public CliErrorException(CliExitCode code, string message, Exception innerException)
+        : base(message, innerException) => Code = code;
     public CliErrorException(CliExitCode code, string message)
-        : base(message)
-    {
-        Code = code;
-    }
+        : base(message) => Code = code;
 
     public CliExitCode Code { get; }
 }
