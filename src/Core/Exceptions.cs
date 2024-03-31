e00ef5d66b8b51fa3300037257d47a317b2d0cd9
Scott Bilas <scottbilas@gmail.com>
2024-03-31 14:13:37 +0100

    Little util

---
+++ src/Core/Exceptions.cs
@@ -0,0 +1,7 @@
+﻿namespace OkTools.Core;
+
+public class UnreachableCodeException : Exception
+{
+    public UnreachableCodeException(string message) : base(message) {}
+    public UnreachableCodeException() : this("Internal error: this code should be unreachable!") {}
+}
