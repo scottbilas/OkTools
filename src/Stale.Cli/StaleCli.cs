da6d4d3977087d1c5c6529dc8caa949506bffc43
Scott Bilas <scottbilas@gmail.com>
2024-03-31 12:32:02 +0100

    Ok clear out all this stuff, we will just copy over as needed from flog

--- src/Stale.Cli/StaleCli.cs
+++ src/Stale.Cli/StaleCli.cs
@@ -1,42 +1,18 @@
-﻿const string programVersion = "0.1";
+﻿using Spectre.Console;
 
-// fine to use console directly here, it's outside of any tui work
-#pragma warning disable RS0030
+const string programVersion = "0.1";
 
-// TODO: add --config prefix before command
-
-var debugMode = false;
-
-var (exitCode, opt) = StaleCliArguments.CreateParser().Parse(args, programVersion, StaleCliArguments.Help, StaleCliArguments.Usage,
-    opts =>
-    {
-        //$$$if (!Enum.TryParse(opts.OptWrap, true, out WrapType _))
-        //$$$    throw new DocoptNet.DocoptInputErrorException("Unrecognized wrap type: " + opts.OptWrap);
-        return null;
-    });
+var (exitCode, opts) = StaleCliArguments.CreateParser().Parse(args, programVersion, StaleCliArguments.Help, StaleCliArguments.Usage);
 if (exitCode != null)
     return (int)exitCode.Value;
 
 try
 {
-    debugMode = opt.OptDebug;
-
-    using var staleApp = new StaleApp(opt);
+    using var staleApp = new StaleApp(opts);
     return (int)await staleApp.Run();
 }
-catch (CliExitException x)
+catch (Exception x)
 {
-    if (x.InnerException != null)
-    {
-        Console.Error.WriteLine(x.Message);
-        if (debugMode)
-        {
-            Console.Error.WriteLine();
-            Console.Error.WriteLine(x.InnerException);
-        }
-    }
-    else
-        Console.Error.WriteLine(x);
-
-    return (int)x.Code;
+    AnsiConsole.WriteException(x, ExceptionFormats.ShortenTypes | ExceptionFormats.ShortenPaths);
+    return (int)CliExitCode.ErrorSoftware;
 }
