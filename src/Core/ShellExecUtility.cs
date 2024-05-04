29f2679e0d16202f4d293e7b6402e868c0611a74
Scott Bilas <scottbilas@gmail.com>
2024-05-04 12:50:40 +0200

    Move shell exec stuff out of the simple cli utility code

--- src/Core/ShellExecUtility.cs
+++ src/Core/ShellExecUtility.cs
@@ -14,6 +14,108 @@ namespace OkTools.Core;
 
 public static class ShellExecUtility
 {
+    public enum StdChannel
+    {
+        Stdout,
+        Stderr
+    }
+
+    // TODO: use initializer struct
+    public static int Execute(
+        string exePath, IEnumerable<object>? processArgs, string? workingDirectory,
+        Action<string, StdChannel>? onLine, IEnumerable<string>? stdinLines = null)
+    {
+        processArgs = processArgs.OrEmpty();
+
+        var processArgsText = processArgs
+            .Select(obj =>
+            {
+                var str = obj.ToString();
+                if (str?.Contains(' ') == true)
+                    str = '"' + str + '"';
+                return str;
+            })
+            .StringJoin(" ");
+
+        using var stdoutCompleted = new ManualResetEvent(false);
+        using var stderrCompleted = new ManualResetEvent(false);
+        using var process = new Process();
+        process.StartInfo = new ProcessStartInfo
+        {
+            // keep new process completely out of user view
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Hidden,
+            ErrorDialog = false,
+
+            WorkingDirectory = workingDirectory ?? Environment.CurrentDirectory,
+            FileName = exePath,
+            Arguments = processArgsText,
+
+            RedirectStandardInput = stdinLines != null,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+        };
+
+        // avoid caller needing to do this (and pretty much everybody will want it)
+        var serializer = new object();
+
+        // ReSharper disable AccessToDisposedClosure
+        // ^ this is ok because we either kill or wait for process to stop before `using` will dispose the events
+        process.OutputDataReceived += (_, line) =>
+        {
+            if (line.Data == null)
+                stdoutCompleted.Set();
+            else if (onLine != null)
+            {
+                lock (serializer)
+                    onLine(line.Data, StdChannel.Stdout);
+            }
+        };
+        process.ErrorDataReceived += (_, line) =>
+        {
+            if (line.Data == null)
+                stderrCompleted.Set();
+            else if (onLine != null)
+            {
+                lock (serializer)
+                    onLine(line.Data, StdChannel.Stderr);
+            }
+        };
+        // ReSharper restore AccessToDisposedClosure
+
+        // start everything
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        // write if needed
+        if (stdinLines != null)
+        {
+            foreach (var line in stdinLines)
+                process.StandardInput.WriteLine(line);
+
+            process.StandardInput.Close();
+        }
+
+        // wait for proc and all reads to finish
+        process.WaitForExit();
+        stdoutCompleted.WaitOne();
+        stderrCompleted.WaitOne();
+
+        return process.ExitCode;
+    }
+
+    public static int Execute(
+        string exePath, IEnumerable<object>? processArgs, string workingDirectory,
+        ICollection<string> stdout, ICollection<string> stderr, IEnumerable<string>? stdin = null)
+    {
+        return Execute(
+            exePath, processArgs, workingDirectory,
+            (line, stream) => (stream == StdChannel.Stdout ? stdout : stderr).Add(line),
+            stdin);
+    }
+
     public static NPath? TryResolveExecutableExtension(string basePath)
     {
         var baseNPath = basePath.ToNPath();
