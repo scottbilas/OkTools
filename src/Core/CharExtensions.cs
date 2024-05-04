c15fc0ef1aa5a78b49ee00b1c24a8f83e38650d5
Scott Bilas <scottbilas@gmail.com>
2024-05-04 12:49:29 +0200

    Make it easier to override (I think?)

---
+++ src/Core/CharExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace OkTools.Core;
+
+public static class CharExtensions
+{
+    public static char ToUpper(this char @this) => char.ToUpperInvariant(@this);
+    public static char ToLower(this char @this) => char.ToLowerInvariant(@this);
+}
