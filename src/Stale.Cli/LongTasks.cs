0d7c2b87163490a24dee81f8fbab8006f55b4cee
Scott Bilas <scottbilas@gmail.com>
2024-07-13 07:23:12 +0100

    A few new utils

---
+++ src/Stale.Cli/LongTasks.cs
@@ -0,0 +1,102 @@
+using System.Diagnostics;
+
+record LongTask(string Name
+#   if ENABLE_TASK_CREATION_TRACKING
+    , StackTrace Creation
+#   endif
+    )
+{
+    static int s_nextId;
+    public readonly int Serial = Interlocked.Increment(ref s_nextId);
+
+    Task? _task;
+    public Task Task
+    {
+        get => _task ?? throw new InvalidOperationException("No task assigned");
+
+        internal set
+        {
+            if (_task != null)
+                throw new InvalidOperationException("Task already assigned");
+            _task = value;
+        }
+    }
+}
+
+class LongTasks
+{
+    readonly Dictionary<int, LongTask> _tasks = new();
+    readonly CancellationToken _stopToken;
+
+    public LongTasks(CancellationToken stopToken)
+    {
+        _stopToken = stopToken;
+    }
+
+    public IReadOnlyList<LongTask> GetTasksSnapshot()
+    {
+        lock (_tasks)
+            return [.._tasks.Values];
+    }
+
+    // use this for fire-and-forget functions that shouldn't fail, but we definitely want to catch when they do so can
+    // improve error handling of them. def worse to have them fail quietly in the background.
+    public LongTask Run(string taskName, Func<CancellationToken, Task> action, CancellationToken? stopToken = null)
+    {
+        stopToken ??= _stopToken;
+
+        // doing this through ctx so that in the future i can do a little nicer handling of a background task
+        // failure, like adding extra supporting data to a log file or whatever.
+
+        var task = new LongTask(taskName, new StackTrace());
+        var assigned = false;
+
+        async Task LongAction()
+        {
+            // just in case it's possible that a very fast task startup may happen before task assignment to the record
+            // ReSharper disable once AccessToModifiedClosure LoopVariableIsNeverChangedInsideLoop
+            while (!assigned)
+                await Task.Delay(0, stopToken.Value);
+
+            lock (_tasks)
+                _tasks.Add(task.Serial, task);
+
+            try
+            {
+                await action(stopToken.Value);
+            }
+            catch (OperationCanceledException)
+            {
+                // ignore
+            }
+            catch (Exception runException)
+            {
+                Environment.FailFast(
+                    $"Failure in {nameof(LongTasks)}.{nameof(Run)}('{taskName}')",
+                    new LongTaskException(task, runException));
+            }
+
+            lock (_tasks)
+                _tasks.Remove(task.Serial);
+        }
+
+        task.Task = Task.Factory
+            .StartNew(LongAction, stopToken.Value, TaskCreationOptions.LongRunning, TaskScheduler.Default)
+            .Unwrap();
+        assigned = true;
+        return task;
+    }
+}
+
+class LongTaskException : Exception
+{
+    readonly LongTask _ltask;
+
+    public LongTaskException(LongTask ltask, Exception runException)
+        : base(ltask.Name, runException)
+    {
+        _ltask = ltask;
+    }
+
+    public override string StackTrace => _ltask.Creation.ToString();
+}
---
+++ src/Stale.Cli/MiscUtils.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+static class MiscUtils
+{
+    public static IEnumerable<string> SequenceDuplicatesAsDots(IEnumerable<string> source)
+    {
+        var last = "";
+        var count = 0;
+
+        string GetLast() => last + new string('.', count == 1 ? 0 : count);
+
+        foreach (var item in source)
+        {
+            if (item == last)
+                ++count;
+            else
+            {
+                if (count > 0)
+                    yield return GetLast();
+
+                count = 1;
+                last = item;
+            }
+        }
+
+        if (count > 0)
+            yield return GetLast();
+    }
+
+    public static string ToNiceMethodName(MethodBase method)
+    {
+        // TODO: make this not crap code
+
+        // not in love with this code. ideally would go look at .net source and try to build a simple little
+        // parser, but who's got time for that. i'm just going to hack on this as needed. probably will have to
+        // adjust it every new .net release anyway...
+
+        var methodName = method.Name;
+
+        // base name
+        var name = (method.DeclaringType!.FullName! + '.' + methodName);
+
+        // not useful to me to have nesting vs namespace differentiation
+        name = name.Replace('+', '.');
+
+        // clean up generated code a little
+        var found = Regex.Match(name, @"<>c__DisplayClass\d+_\d+\.");
+        var isLambda = found.Success;
+        if (found.Success)
+        {
+            var replaced = name[(found.Index + found.Length)..]
+                .RegexReplace(@"<<(.*)>b__\d+>d", "$1")
+                .RegexReplace(@"<(.*)>b__\d+", "$1");
+
+            // detect when we have a top level function
+            var last = replaced;
+            replaced = replaced.RegexReplace(@"<(.*)>g__(.*)\|\d+", "$1.$2");
+            if (replaced != last)
+                isLambda = false;
+
+            name = name[..found.Index] + replaced;
+        }
+
+        // more simplifying of naming
+        name = name
+            .RegexReplace(@"^Program\.", "")
+            .RegexReplace(@"^<Main>\$\.", "")
+            .Replace("<Main>$", "main");
+
+        // a bit of markup
+        name = name.RegexReplace(@"(^|\.)MoveNext$", "⇓");
+        if (isLambda)
+            name = 'λ' + name;
+
+        return name;
+    }
+}
