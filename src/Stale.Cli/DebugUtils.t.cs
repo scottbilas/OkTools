7c8716a156f678100101c154d643b301dc1103f9
Scott Bilas <scottbilas@gmail.com>
2024-09-15 07:14:15 +1000

    New test project for stale

---
+++ src/Stale.Cli/DebugUtils.t.cs
@@ -0,0 +1,23 @@
+﻿class DebugUtilsTests
+{
+    [TestCase("abc",         "abc")]
+    [TestCase("abc;def",     "abc;def")]
+    [TestCase("abc;abc;def", "abc.;def")]
+    [TestCase("abc;def;abc", "abc;def;abc")]
+    [TestCase("abc;def;def;abc", "abc;def.;abc")]
+    [TestCase("abc;def;def;def;def;abc", "abc;def...;abc")]
+    [TestCase("abc;def;abc;abc;abc", "abc;def;abc..")]
+    public void SequenceDuplicatesAsDots(string given, string expected)
+    {
+        var givenArray = given.Split(';');
+        var expectedArray = expected.Split(';');
+
+        DebugUtils.SequenceDuplicatesAsDots(givenArray).ShouldBe(expectedArray);
+    }
+
+    [Test]
+    public void SequenceDuplicatesAsDots_WithEmpties()
+    {
+        DebugUtils.SequenceDuplicatesAsDots([]).ShouldBe([]);
+    }
+}
