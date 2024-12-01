fcdd32b44f7dcfe48b0d303a93c3da435470487c
Scott Bilas <scottbilas@gmail.com>
2024-12-01 17:50:55 +0000

    Add some API tracking
    Terminal doesn't work yet, causes the tool to crash.

--- pre-publish.ps1
+++ pre-publish.ps1
@@ -1,27 +1,43 @@
 [CmdletBinding()]
 param (
     [switch]$Core,
-    [switch]$Terminal)
+    [switch]$Terminal,
+    [switch]$All)
+
+if ($All)
+{
+    $Core = $true
+    $Terminal = $true
+}
 
 $rc = 0
 
-if ($Core) {
-    "*** BUILDING CORE ***"
-    dotnet build src/Core/Core.csproj -c Release --nologo
+filter fix-bug {
+    # https://github.com/dotnet/msbuild/issues/10998
+    $esc = [char]27
+    $_.replace("$esc]9;4;3;$esc\$esc]9;4;0;$esc\", '')
+}
+
+function dowit($wat) {
+    "*** BUILDING $($wat.ToUpper()) ***"
+    dotnet build src/$wat/$wat.csproj -c Release --nologo
     if (!$LASTEXITCODE) {
-        dotnet test src/Core/Core-Tests.csproj --nologo
+        dotnet test src/$wat/$wat-Tests.csproj --nologo
+    }
+    if (!$LASTEXITCODE) {
+        dotnet pack src/$wat/$wat.csproj -c Release
+    }
+    if (!$LASTEXITCODE) {
+        dotnet tool restore
+    }
+    if (!$LASTEXITCODE) {
+        dotnet tool run generate-public-api --target-frameworks net9.0 --assembly (resolve-path artifacts\build\bin\$wat\net9.0\OkTools.$wat.dll) | fix-bug > api-$($wat.ToLower()).txt
     }
     if ($LASTEXITCODE) { $rc = 1 }
     ""
 }
 
-if ($Terminal) {
-    dotnet build src/Terminal/Terminal.csproj -c Release --nologo
-    if (!$LASTEXITCODE) {
-        dotnet test src/Terminal/Terminal-Tests.csproj --nologo
-    }
-    if ($LASTEXITCODE) { $rc = 1 }
-    ""
-}
+if ($Core) { dowit Core }
+if ($Terminal) { dowit Terminal }
 
 exit $rc
