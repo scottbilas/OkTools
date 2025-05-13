db2b85af51d0e5b977294738ea8b84dd0da98955
Scott Bilas <scottbilas@gmail.com>
2025-05-13 10:24:54 +0100

    Not ready for 9 analyzers yet

---
+++ src/Core/Extensions/EnumerableExtensions_Dictionary.cs
@@ -0,0 +1,67 @@
+﻿namespace OkTools.Core.Extensions;
+
+public static partial class EnumerableExtensions
+{
+#   if !NET8_0_OR_GREATER
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<(TKey key, TValue value)> @this) where TKey: notnull =>
+        @this.ToDictionary(item => item.key, item => item.value);
+#   endif
+
+    public static Dictionary<TKey, List<TValue>> ToMultiDictionary<TKey, TValue>(
+        this IEnumerable<(TKey key, TValue value)> @this) where TKey: notnull
+    {
+        var dict = new Dictionary<TKey, List<TValue>>();
+        foreach (var (key, value) in @this)
+        {
+            if (!dict.TryGetValue(key, out var list))
+                dict.Add(key, list = []);
+            list.Add(value);
+        }
+        return dict;
+    }
+
+    public static Dictionary<TKey, List<TValue>> ToMultiDictionary<TKey, TValue>(
+        this IEnumerable<TValue> @this, Func<TValue, TKey> keySelector) where TKey: notnull
+    {
+        var dict = new Dictionary<TKey, List<TValue>>();
+        foreach (var item in @this)
+        {
+            var key = keySelector(item);
+            if (!dict.TryGetValue(key, out var list))
+                dict.Add(key, list = []);
+            list.Add(item);
+        }
+        return dict;
+    }
+
+    public static Dictionary<TKey, List<TValue>> ToMultiDictionary<TItem, TKey, TValue>(
+        this IEnumerable<TItem> @this, Func<TItem, TKey> keySelector, Func<TItem, TValue> valueSelector) where TKey: notnull
+    {
+        var dict = new Dictionary<TKey, List<TValue>>();
+        foreach (var item in @this)
+        {
+            var key = keySelector(item);
+            if (!dict.TryGetValue(key, out var list))
+                dict.Add(key, list = []);
+            list.Add(valueSelector(item));
+        }
+        return dict;
+    }
+
+    public static IDictionary<TKey, List<TValue>> ToDefaultMultiDictionary<TKey, TValue>(
+        this IEnumerable<(TKey key, TValue value)> @this) where TKey: notnull =>
+            @this.ToMultiDictionary().ToDefaultDictionary();
+    public static IDictionary<TKey, List<TValue>> ToDefaultMultiDictionary<TKey, TValue>(
+        this IEnumerable<TValue> @this, Func<TValue, TKey> keySelector) where TKey: notnull =>
+            @this.ToMultiDictionary(keySelector).ToDefaultDictionary();
+    public static IDictionary<TKey, List<TValue>> ToDefaultMultiDictionary<TItem, TKey, TValue>(
+        this IEnumerable<TItem> @this, Func<TItem, TKey> keySelector, Func<TItem, TValue> valueSelector) where TKey: notnull =>
+            @this.ToMultiDictionary(keySelector, valueSelector).ToDefaultDictionary();
+
+    public static Dictionary<TKey, List<TValue>> ToMultiDictionary<TKey, TValue>(
+        this IEnumerable<IGrouping<TKey, TValue>> @this) where TKey: notnull =>
+            @this.ToDictionary(g => g.Key, g => g.ToList());
+    public static IDictionary<TKey, List<TValue>> ToDefaultMultiDictionary<TKey, TValue>(
+        this IEnumerable<IGrouping<TKey, TValue>> @this) where TKey: notnull =>
+            @this.ToMultiDictionary().ToDefaultDictionary();
+}
---
+++ src/Core/Extensions/EnumerableExtensions_Tuple.cs
@@ -0,0 +1,94 @@
+﻿namespace OkTools.Core.Extensions;
+
+public static partial class EnumerableExtensions
+{
+    public static (T a, T b) ToTuple2<T>(this IEnumerable<T> @this)
+    {
+        using var e = @this.GetEnumerator();
+
+        e.MoveNext();
+        var t0 = e.Current;
+        e.MoveNext();
+        var t1 = e.Current;
+
+        var tuple = ValueTuple.Create(t0, t1);
+        if (e.MoveNext())
+            throw new InvalidOperationException("More than 2 elements found");
+        return tuple;
+    }
+
+    public static IEnumerable<(T a, T b)> SelectTuple2<T>(this IEnumerable<T> @this)
+    {
+        using var e = @this.GetEnumerator();
+        while (e.MoveNext())
+        {
+            var a = e.Current;
+            e.MoveNext();
+            yield return (a, e.Current);
+        }
+    }
+
+    public static (T a, T b, T c) ToTuple3<T>(this IEnumerable<T> @this)
+    {
+        using var e = @this.GetEnumerator();
+
+        e.MoveNext();
+        var t0 = e.Current;
+        e.MoveNext();
+        var t1 = e.Current;
+        e.MoveNext();
+        var t2 = e.Current;
+
+        var tuple = ValueTuple.Create(t0, t1, t2);
+        if (e.MoveNext())
+            throw new InvalidOperationException("More than 3 elements found");
+        return tuple;
+    }
+
+    public static IEnumerable<(T a, T b, T c)> SelectTuple3<T>(this IEnumerable<T> @this)
+    {
+        using var e = @this.GetEnumerator();
+        while (e.MoveNext())
+        {
+            var a = e.Current;
+            e.MoveNext();
+            var b = e.Current;
+            e.MoveNext();
+            yield return (a, b, e.Current);
+        }
+    }
+
+    public static (T a, T b, T c, T d) ToTuple4<T>(this IEnumerable<T> @this)
+    {
+        using var e = @this.GetEnumerator();
+
+        e.MoveNext();
+        var t0 = e.Current;
+        e.MoveNext();
+        var t1 = e.Current;
+        e.MoveNext();
+        var t2 = e.Current;
+        e.MoveNext();
+        var t3 = e.Current;
+
+        var tuple = ValueTuple.Create(t0, t1, t2, t3);
+        if (e.MoveNext())
+            throw new InvalidOperationException("More than 4 elements found");
+        return tuple;
+    }
+
+    public static IEnumerable<(T a, T b, T c, T d)> SelectTuple4<T>(this IEnumerable<T> @this)
+    {
+        using var e = @this.GetEnumerator();
+        while (e.MoveNext())
+        {
+            var a = e.Current;
+            e.MoveNext();
+            var b = e.Current;
+            e.MoveNext();
+            var c = e.Current;
+            e.MoveNext();
+            yield return (a, b, c, e.Current);
+        }
+    }
+}
